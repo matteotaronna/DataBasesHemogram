f5a86682ba5a339a6ecfc4248b39abb8d1b844d6
jaime <jaime@10.60.58.29>
2020-03-09 17:47:14 +0100

    3rd commit of the day :)

--- src/hemogram/db/analyzer/SQLCreateAnalyzer.java
+++ src/hemogram/db/analyzer/SQLCreateAnalyzer.java
@@ -1,5 +1,35 @@
 package hemogram.db.analyzer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class SQLCreateAnalyzer {
-	
+	public static void main(String args[]) {
+		try {
+			// Open database connection
+			Class.forName("org.sqlite.JDBC");
+			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/HemogramDB.db");
+			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			System.out.println("Database connection opened.");
+			
+			Statement stmt3 = c.createStatement();
+			String sql3 = "CREATE TABLE analizers "
+					   + "(id         INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					   + " name       TEXT     NOT NULL, "
+					   + " surname    TEXT     NOT NULL, "
+					   + " work_user  TEXT     NOT NULL UNIQUE,"
+					   + " hospital   TEXT     NOT NULL,"
+					   + " patient_id INTEGER REFERENCES patients(id) ON UPDATE CASCADE ON DELETE SET NULL)";
+			stmt3.executeUpdate(sql3);
+			stmt3.close();
+			
+			
+			// Close database connection
+			c.close();
+			System.out.println("Database connection closed.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
