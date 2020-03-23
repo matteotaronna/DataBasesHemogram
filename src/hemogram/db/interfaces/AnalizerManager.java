6825179a062ff55dda4ad7587a252949c3c4b419
claudia <claudia@localhost>
2020-03-23 18:38:08 +0100

    nfjwbef

--- src/hemogram/db/interfaces/AnalizerManager.java
+++ src/hemogram/db/interfaces/AnalizerManager.java
@@ -6,6 +6,5 @@ public interface AnalizerManager
 {
 	public void insertAnalizer(Analizer analizer);
 	public Analizer getAnalizer (int analizerId);
-	public int getAnalizerId (Analizer analizer);
 	public Analizer signUpAnalizer (String name, String work_user);
 }
