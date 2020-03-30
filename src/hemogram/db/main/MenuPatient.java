ab2c840078c47e078ef96b67675050d1ea15132b
matet <matet@MatteoPC>
2020-03-30 18:30:07 +0200

    nbcx

--- src/hemogram/db/main/MenuPatient.java
+++ src/hemogram/db/main/MenuPatient.java
@@ -4,23 +4,4 @@ import hemogram.db.pojos.Patient;
 
 public class MenuPatient {
 	
-	public static void patientMenu()
-	{
-		try
-		{
-			while(true)
-			{
-				Patient patient;
-				System.out.println("Introduce your name");
-				String patientname= reader.readLine();
-				System.out.println("Introduce your DNI");
-				String patientDNI =reader.readLine();
-				
-				
-			}
-		}catch(Exception e)
-		{
-				e.printStackTrace();
-		}
-	}
 }
